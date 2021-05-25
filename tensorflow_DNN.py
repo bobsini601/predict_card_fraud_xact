9fb4189e6ab7aeac8dfcf1b839989c981908439d
gmlwn4009 <gmlwn4009@naver.com>
2021-05-25 19:17:19 +0900

    tensorflow heeju

--- tensorflow_DNN.py
+++ tensorflow_DNN.py
@@ -16,24 +16,27 @@ def get_CSV(load):
     data2 = data1.to_numpy()
     return data2
 
-
 X_train = get_CSV('X_train.csv')
 y_train = get_CSV('y_train.csv')
 X_test = get_CSV('X_test.csv')
 y_test = get_CSV('y_test.csv')
-# validation_data = get_CSV('validation.csv')
+validation_data = get_CSV('validation.csv')
+
+x_val = validation_data[:,2:31]
+y_val = validation_data[:,31]
 
 x_train = X_train[:,2:]  #index랑 time 빼기
 x_test = X_test[:,2:]
 
-print(y_train[:,1])
-
 x_train_normal = preprocessing.normalize(x_train,norm='l1')
 x_test_normal = preprocessing.normalize(x_test,norm='l1')
-# sm = SMOTE(sampling_strategy=0.3)
-# X, Y = sm.fit_resample(x_train,Y.ravel())
+x_val_normal = preprocessing.normalize(x_val,norm='l1')
 
-input = keras.Input(shape=(x_train_normal.shape[1],))
+sm = SMOTE(sampling_strategy=1)
+X, Y = sm.fit_resample(x_train_normal,y_train[:,1])
+x_t, y_t = sm.fit_resample(x_test_normal,y_test[:,1])
+
+input = keras.Input(shape=(29,))
 x=Dense(15,activation='relu')(input)
 output=Dense(1,activation='sigmoid')(x)
 
@@ -41,7 +44,7 @@ model = keras.Model(input, output)
 model.summary()
 
 model.compile(loss='binary_crossentropy',optimizer=keras.optimizers.Adam(lr=1e-3),metrics=['accuracy']) #binary_crossentropy
-model.fit(x_train_normal,y_train[:,1],epochs=10,batch_size=x_train.shape[0])
+model.fit(x_train_normal,y_train[:,1],epochs=10,batch_size=x_train_normal.shape[0], validation_data=(x_val_normal,y_val))
 # model.summary()
-score = model.evaluate(x_test_normal,y_test[:,1],batch_size=1)
+score = model.evaluate(x_test,y_test[:,1],batch_size=1)
 print(score)  #loss & accuracy 출력
